adba7709a506ebd024ab1470cccb748a8a587aa5
ScriptkidHicks <tammashicks@gmail.com>
2022-07-05 14:36:00 -0700

    updating keyboard popup

--- todolist/App.js
+++ todolist/App.js
@@ -1,5 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
 import Task from "./components/Task";
 
 export default function App() {
@@ -15,6 +23,19 @@ export default function App() {
           <Task text={"Task 2"} />
         </View>
       </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writerTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={"Write a task"} />
+
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -23,10 +44,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#E8EAED",
+    position: "relative",
   },
 
   Taskwrapper: {
-    paddingTop: 80,
+    paddingTop: 40,
     paddingHorizontal: 20,
   },
 
@@ -35,5 +57,39 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
 
-  items: {},
+  items: {
+    marginTop: 30,
+  },
+
+  writerTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    left: 20,
+    width: "90%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    backgroundColor: "white",
+    borderRadius: 60,
+    textAlign: "center",
+    borderColor: "#c0c0c0",
+    borderWidth: 1,
+  },
+
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "white",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#c0c0c0",
+    borderWidth: 1,
+  },
 });
